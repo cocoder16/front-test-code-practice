275f2c500305f08bde4c1abb2ea03a38a0f84d8b
cocoder16 <60813614+cocoder16@users.noreply.github.com>
2021-11-27 17:29:28 +0900

    [Add] rerender Item checked when click checkbox test

--- src/domains/toDoList/item/Item.spec.tsx
+++ src/domains/toDoList/item/Item.spec.tsx
@@ -20,7 +20,7 @@ describe("Item component", () => {
       />
     );
 
-    cy.get("div.todo-item").should(
+    cy.get(".todo-item").should(
       "have.css",
       "border-top",
       "1px solid rgb(0, 0, 0)"
@@ -46,7 +46,7 @@ describe("Item component", () => {
       />
     );
 
-    cy.get("div.todo-item").should(
+    cy.get(".todo-item").should(
       "have.css",
       "border-top",
       "0px none rgb(0, 0, 0)"
@@ -54,11 +54,31 @@ describe("Item component", () => {
     cy.get("input[type='checkbox']").should("be.checked");
   });
 
+  it("rerender Item checked when click checkbox", () => {
+    const props = {
+      id: 1,
+      order: 1,
+      content: "Study cypress",
+      checked: false,
+    };
+
+    mount(
+      <Item
+        id={props.id}
+        order={props.order}
+        content={props.content}
+        checked={props.checked}
+      />
+    );
+
+    cy.get(".todo-item input[type='checkbox']").as("checkbox").click();
+    cy.get("@checkbox").should("be.checked");
+  });
+
   // TODO:
   // - 여러 props 값에 대한 렌더링 테스트 V
   // - store domain별로? (mobX) 아님 전역으로 1개? (redux) => choose redux & 1 store V
   // - redux 파일 구조 V
-  // - check event handling -> 핸들러 함수를 컴포넌트 내 정의 -> 호출하는지 테스트 -> 핸들러 함수가 리덕스 액션을 호출하는지 테스트
   // - reducer 함수 유닛테스트
-  // - rerendering 기능 테스트
+  // - rerendering 기능 테스트 (리덕스 없이 V, 리덕스 끼고 재통과해야함)
 });
