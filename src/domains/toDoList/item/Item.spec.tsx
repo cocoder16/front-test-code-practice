1fe9e616729c8d976cfc9100d9a1a67cadff0c1f
cocoder16 <60813614+cocoder16@users.noreply.github.com>
2021-11-27 17:05:40 +0900

    [Update] render item with checked

--- src/domains/toDoList/item/Item.spec.tsx
+++ src/domains/toDoList/item/Item.spec.tsx
@@ -3,7 +3,7 @@ import { mount } from "@cypress/react";
 import Item from "./Item";
 
 describe("Item component", () => {
-  it("render first order Item which have border-top", () => {
+  it("render first order Item which have border-top and no checked", () => {
     const props = {
       id: 0,
       order: 0,
@@ -29,12 +29,12 @@ describe("Item component", () => {
     cy.get("input[type='checkbox']").should("not.be.checked");
   });
 
-  it("render second order Item which don't have border-top", () => {
+  it("render second order Item which don't have border-top and checked", () => {
     const props = {
       id: 1,
       order: 1,
       content: "Study cypress",
-      checked: false,
+      checked: true,
     };
 
     mount(
@@ -51,13 +51,14 @@ describe("Item component", () => {
       "border-top",
       "0px none rgb(0, 0, 0)"
     );
+    cy.get("input[type='checkbox']").should("be.checked");
   });
 
   // TODO:
   // - 여러 props 값에 대한 렌더링 테스트 V
-  // - redux store domain별로? 아님 전역으로 1개?
-  // - redux 파일 구조
+  // - store domain별로? (mobX) 아님 전역으로 1개? (redux) => choose redux & 1 store V
+  // - redux 파일 구조 V
   // - check event handling -> 핸들러 함수를 컴포넌트 내 정의 -> 호출하는지 테스트 -> 핸들러 함수가 리덕스 액션을 호출하는지 테스트
-  // - redux action dispatch 했을때, reducer 호출하는지 테스트
   // - reducer 함수 유닛테스트
+  // - rerendering 기능 테스트
 });
