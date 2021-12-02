ab38f7245df01d2c7339ec24aba50bff157c7882
cocoder16 <60813614+cocoder16@users.noreply.github.com>
2021-12-02 16:23:46 +0900

    [Install] typescript-eslint/parser
    [Update] scripts

--- src/store/toDoList/reducers.spec.ts
+++ src/store/toDoList/reducers.spec.ts
@@ -42,4 +42,19 @@ describe("to do list reducers", () => {
       expectedResult
     );
   });
+
+  // it("get all to-do-list data", () => {
+  //   const action: IGetAllAction = {
+  //     payload: initialState,
+  //   };
+  //   expect(
+  //     reducer.getAll({ toDoList: [] }, action).to.deep.equal(initialState)
+  //   );
+  // });
+
+  // TODO: code coverage 확인하는 세팅
+  // TODO: axios get to do list data - 1) reducer unit test - 2) axios mocking 포함한 기능 테스트
+  // TODO: 1) check 클릭했을때 기능 테스트
+  // TODO: axios 응답 실패했을때 이후 로직
+  // TODO: addDefaultCase 공통 함수로
 });
