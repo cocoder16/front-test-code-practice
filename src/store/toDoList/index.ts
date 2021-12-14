c045594c75d8733c7f53bc6b0eaf63a6ef84f0f4
cocoder16 <cocoder16@gmail.com>
2021-12-14 20:26:46 +0900

    [Add] reducer when rejected

--- src/store/toDoList/index.ts
+++ src/store/toDoList/index.ts
@@ -11,7 +11,7 @@ export const action = {
       return service
         .postToDo(payload)
         .then(response => response.data)
-        .catch(error => rejectWithValue(error.response.data));
+        .catch(error => rejectWithValue(error.response.data)); // 400, 500번대 이거나 요청이 failed 일때, catch로 들어옴
     },
   ),
   getAll: createAsyncThunk(`${actionPrefix}/GET_ALL`, async (_, { rejectWithValue }) => {
@@ -44,6 +44,18 @@ export const reducer = {
     state.toDoList = action.payload.toDoList;
     return state;
   },
+  handleReject: (state: IToDoListState, action: any) => {
+    state.toDoList = [
+      {
+        id: 99,
+        order: 99,
+        content: "!!",
+        checked: true,
+      },
+    ];
+    // 실패한 요청에 대해서는 리덕스에서 딱히 처리해주고 싶은 로직이 없는데?
+    // 뭐이런것도 가능은 함.
+  },
   updateChecked: (state: IToDoListState, action: IUpdateCheckedAction) => {
     state.toDoList.find((todo: ToDo) => todo.id === action.payload.id).checked = action.payload.checked;
     return state;
@@ -58,6 +70,7 @@ const toDoListReducer = createReducer(initialState, builder => {
   builder
     .addCase(action.postToDo.fulfilled, reducer.postToDo)
     .addCase(action.getAll.fulfilled, reducer.getAll)
+    .addCase(action.getAll.rejected, reducer.handleReject) // 400, 500번대 이거나 요청이 failed 일때, rejected로 들어옴
     .addCase(action.updateChecked.fulfilled, reducer.updateChecked)
     .addCase(action.deleteToDo.fulfilled, reducer.deleteToDo);
 });
